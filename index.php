a97d0f0a85c7d168a2145d5cc6894162e966330d
Рамиль Шарипов <r.sharipov@legacystudio.ru>
2019-07-11 21:03:12 +0500

    Добавил индексную страницу с функцией автозагрузки классов для тестов

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+// Добавляем корень сайта в пути, по которым будут искаться классы
+set_include_path( get_include_path() . PATH_SEPARATOR . __DIR__);
+
+// Функция автозагрузки
+function namespaceAutoload( $path ) {
+    // Заменяем символы разделяющие директории на поддерживаемые OS
+    if ( preg_match( '/\\\\/', $path ) ) {
+        $path = str_replace( '\\', DIRECTORY_SEPARATOR, $path );
+    }
+
+    $file = stream_resolve_include_path( "$path.php" );
+
+    if ( file_exists( $file ) ) {
+        require_once( "$file" );
+    } else {
+        $class = substr( strrchr( $path, DIRECTORY_SEPARATOR ), 1 );
+        $search = str_split( $class );
+
+        $start = null;
+        $end = null;
+
+        foreach ( $search as $key => $letter ) {
+            if ( $letter === strtoupper( $letter ) )
+                if ( is_null( $start ) )
+                    $start = $key;
+                else if ( ! is_null( $start ) && is_null( $end ) )
+                    $end = $key;
+                else
+                    break;
+        }
+
+        $newClass = substr( $class, $end );
+        $newPath = str_replace( $class, $newClass, $path );
+
+        $file = stream_resolve_include_path( "$newPath.php" );
+
+        if ( file_exists( $file ) ) {
+            require_once( "$file" );
+        }
+    }
+}
+
+spl_autoload_register( 'namespaceAutoload' );
